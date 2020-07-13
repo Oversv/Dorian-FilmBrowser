6c477b00902658d10ac2f0747757e0085d79bff4
Oversv <albertomdv@gmail.com>
2020-07-13 09:39:57 +0100

    fix access denied

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,13 +1,17 @@
 "use strict";
 
-function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
-
 /** Info Header **/
 var welcome = document.getElementById('welcome');
 var user = JSON.parse(sessionStorage.getItem('user'));
-if (user === undefined) user = (_readOnlyError("user"), 'stranger');
-welcome.textContent = "Welcome ".concat(user.username);
+
+if (user === undefined || user === null) {
+  sessionStorage.clear();
+  window.location.href = "./index.html";
+} else {
+  welcome.textContent = "Welcome ".concat(user.username);
+}
 /** Logout **/
+
 
 var logoutBtn = document.getElementById('btn-logout');
 logoutBtn.addEventListener('click', function () {
--- src/js/logout.js
+++ src/js/logout.js
@@ -2,9 +2,14 @@
 const welcome = document.getElementById('welcome')
 
 const user = JSON.parse(sessionStorage.getItem('user'))
-if(user === undefined) user = 'stranger'
+if(user === undefined || user === null){
+    sessionStorage.clear();
+    window.location.href = "./index.html"
+}
+else{
 
-welcome.textContent = `Welcome ${user.username}`
+    welcome.textContent = `Welcome ${user.username}`
+}
 
 
 /** Logout **/
